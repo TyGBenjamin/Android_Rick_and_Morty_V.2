04d99dd3d70ebc677a9ca3ab16d458172e7194fb
Tyler <tyler.g@ravebizz.com>
2022-10-19 14:12:11 -0400

    working on location Page

--- lib_data/src/main/java/com/example/lib_data/data/repository/RepositoryImpl.kt
+++ lib_data/src/main/java/com/example/lib_data/data/repository/RepositoryImpl.kt
@@ -1,8 +1,10 @@
 package com.example.lib_data.data.repository
 
+import android.location.Location
 import com.example.lib_data.data.remote.ApiService
 import com.example.lib_data.domain.models.CharWrapper
 import com.example.lib_data.domain.models.Data
+import com.example.lib_data.domain.models.Episode
 import com.example.lib_data.domain.repository.Repository
 import com.example.lib_data.utils.Resource
 import kotlinx.coroutines.Dispatchers
@@ -28,6 +30,34 @@ class RepositoryImpl @Inject constructor(private var apiInstance: ApiService) : 
         withContext(Dispatchers.IO) {
             return@withContext try {
                 val res = apiInstance.getCharacterById(id)
+                if (res.isSuccessful && res.body() != null) {
+                    Resource.Success(res.body()!!)
+                } else {
+                    Resource.Error("Someting Wrong")
+                }
+            } catch (e: Exception) {
+                Resource.Error(e.message.toString())
+            }
+        }
+
+    override suspend fun getEpisodeById(id: Int): Resource<Episode> =
+        withContext(Dispatchers.IO) {
+            return@withContext try {
+                val res = apiInstance.getEpisodeById(id)
+                if (res.isSuccessful && res.body() != null) {
+                    Resource.Success(res.body()!!)
+                } else {
+                    Resource.Error("Someting Wrong")
+                }
+            } catch (e: Exception) {
+                Resource.Error(e.message.toString())
+            }
+        }
+
+    override suspend fun getLocationById(id: Int): Resource<Location> =
+        withContext(Dispatchers.IO) {
+            return@withContext try {
+                val res = apiInstance.getLocationById(id)
                 if (res.isSuccessful && res.body() != null) {
                     Resource.Success(res.body()!!)
                 } else {
--- lib_data/src/main/java/com/example/lib_data/domain/repository/Repository.kt
+++ lib_data/src/main/java/com/example/lib_data/domain/repository/Repository.kt
@@ -1,12 +1,16 @@
 package com.example.lib_data.domain.repository
 
+import android.location.Location
 import com.example.lib_data.domain.models.CharWrapper
 import com.example.lib_data.domain.models.Data
 import com.example.lib_data.utils.Resource
 import com.example.lib_data.domain.models.Character
+import com.example.lib_data.domain.models.Episode
 
 interface Repository {
     suspend fun getCharacters() : Resource<Data>
     suspend fun getCharacterById(id:Int) : Resource<Character>
+    suspend fun getEpisodeById(id:Int) : Resource<Episode>
+    suspend fun getLocationById(id:Int) : Resource<Location>
 //    suspend fun getAnimeById(id: String): Resource<AnimeWrapper>
 }
